d49277f26b67ae0abcf5229b5c35351ec9cea46a
Daria Pakshina <daradasa7@yandex.ru>
2024-10-30 09:11:25 +0300

    animate square

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
@@ -5,12 +12,38 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [NgFor],
-  animations: [],
+  animations: [
+    trigger('divState', [
+      state(
+        'normal',
+        style({
+          'background-color': 'red',
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'highlighted',
+        style({
+          'background-color': 'blue',
+          transform: 'translateX(100px)',
+        })
+      ),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+    ]),
+  ],
   styleUrl: './app.component.css',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state == 'normal'
+      ? (this.state = 'highlighted')
+      : (this.state = 'normal');
+  }
 
   onAdd(item: string) {
     this.list.push(item);
